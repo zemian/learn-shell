210c3efbb11f55d3dc6d16dcfe137d50264cb0d6
Zemian Deng <zemiandeng@gmail.com>
2020-11-19 16:53:53 -0500

    Add string and position params test

--- functions.sh
+++ functions.sh
@@ -1,36 +1,49 @@
-# https://www.gnu.org/software/bash/manual/html_node/Shell-Functions.html
+# # https://www.gnu.org/software/bash/manual/html_node/Shell-Functions.html
+#
+# # Use function keyword
+# function foo {
+#   echo "Inside foo function"
+# }
+# # When calling function, we do not use paranthese!
+# foo
+#
+# function foo_params {
+#   echo "param1: $1"
+#   echo "param2: $2"
+#   echo "all params: $@"
+# }
+# foo_params one two three
+#
+# # Another alternate form
+# bar() {
+#   echo "We can use the parantheses in fuction declaration."
+# }
+# bar
+#
+# # Optional arguments
+# function opt_args {
+#   FIRST_ARG=${1:-HELLO}
+#   echo "FIRST_ARG=$FIRST_ARG"
+# }
+# opt_args
+# opt_args 'one'
+#
+# # Argument count
+# function opt_count {
+#   echo "args count $#"
+# }
+# opt_count
+# opt_count 1 2 3 4 5
 
-# Use function keyword
+## Positional arguments
 function foo {
-  echo "Inside foo function"
-}
-# When calling function, we do not use paranthese!
-foo
+  echo '$@='"$@"
+  echo '$#='"$#"
+  echo '$1='"$1"
+  echo '$2='"$2"
+  echo '${10}='"${10}"
 
-function foo_params {
-  echo "param1: $1"
-  echo "param2: $2"
-  echo "all params: $@"
+  # var expansion with position argument - strip prefix
+  echo '${2#t}='"${2#t}"
 }
-foo_params one two three
-
-# Another alternate form
-bar() {
-  echo "We can use the parantheses in fuction declaration."
-}
-bar
-
-# Optional arguments
-function opt_args {
-  FIRST_ARG=${1:-HELLO}
-  echo "FIRST_ARG=$FIRST_ARG"
-}
-opt_args
-opt_args 'one'
-
-# Argument count
-function opt_count {
-  echo "args count $#"
-}
-opt_count
-opt_count 1 2 3 4 5
+foo one two threee 9 8 7 0 1 2 3 4 5 6 7 8 9
--- strings.sh
+++ strings.sh
@@ -1,13 +1,34 @@
-## How to get file extension
-# https://stackoverflow.com/questions/965053/extract-filename-and-extension-in-bash
-fullfile=/tmp/test.txt
-filename=$(basename -- "$fullfile")
-extension="${filename##*.}"
-filename="${filename%.*}"
-echo "filename=$filename"
-echo "extension=$extension"
+# ## How to get file extension
+# # https://stackoverflow.com/questions/965053/extract-filename-and-extension-in-bash
+# fullfile=/tmp/test.txt
+# filename=$(basename -- "$fullfile")
+# extension="${filename##*.}"
+# filename="${filename%.*}"
+# echo "filename=$filename"
+# echo "extension=$extension"
+#
+# ## How to get substring
+# string=01234567890abcdefgh
+# echo ${string:7} # 7890abcdefgh
+# echo ${string:7:2} # 78
 
-## How to get substring
-string=01234567890abcdefgh
-echo ${string:7} # 7890abcdefgh
-echo ${string:7:2} # 78
+# ## Remove prefix string
+# foo='mydata'
+# foo2=${foo#my}
+# foo3=${foo#"my"}
+# echo "foo=$foo"
+# echo "foo2=$foo2"
+# echo "foo3=$foo3"
+#
+# ## Remove suffix string
+# foo='mydata'
+# foo2=${foo%data}
+# foo3=${foo%"data"}
+# echo "foo=$foo"
+# echo "foo2=$foo2"
+# echo "foo3=$foo3"
+
+## Replace string
+foo='mydata'
+foo2=${foo/da/Da}
+echo "foo2=$foo2"
